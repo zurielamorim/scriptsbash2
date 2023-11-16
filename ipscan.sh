bfd077704ec1d263e7fffeaec611ebf0fb74bc08
Zuriel Amorim <38529015+zurielamorim@users.noreply.github.com>
2023-11-16 17:09:45 -0300

    Update ipscan.sh

--- ipscan.sh
+++ ipscan.sh
@@ -19,23 +19,19 @@ output_file="$script_dir/resultado_scan.txt"
 hosts=$(fping -a -g $subnet)
 
 # Salvar a lista de hosts ativos em um arquivo
-echo "Hosts Ativos:" > "$output_file"
-echo "$hosts" >> "$output_file"
-
-# Usar arp para obter os endereços MAC dos hosts ativos
-echo -e "\nInformações Detalhadas:" >> "$output_file"
+echo "Informações Detalhadas:" > "$output_file"
 for host in $hosts; do
     mac=$(arp -n $host | awk '{print $3}')
     
     # Usar o comando curl para consultar o banco de dados OUI da IEEE
-    manufacturer=$(curl -s "https://macvendors.com/query/$mac")
+    manufacturer_info=$(curl -s "https://macvendors.com/query/$mac")
     
     # Verificar se a resposta contém um campo "result" válido
-    if [[ $manufacturer =~ "result" ]]; then
-        company=$(echo "$manufacturer" | jq -r '.result.company')
-        echo "$host : $mac : $company" >> "$output_file"
+    if [[ $manufacturer_info =~ "result" ]]; then
+        company=$(echo "$manufacturer_info" | jq -r '.result.company')
+        echo "IP: $host | MAC: $mac | Fabricante: $company" >> "$output_file"
     else
-        echo "$host : $mac : Informação do fabricante não disponível" >> "$output_file"
+        echo "IP: $host | MAC: $mac | Fabricante: Informação não disponível" >> "$output_file"
     fi
 done
 
