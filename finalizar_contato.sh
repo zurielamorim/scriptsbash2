fd463f4263fce1704a87aab4bdbc71e31a76672e
Zuriel Amorim <38529015+zurielamorim@users.noreply.github.com>
2023-11-28 16:07:05 -0300

    Update finalizar_contato.sh

--- finalizar_contato.sh
+++ finalizar_contato.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Solicitar o número de contato ao usuário
-read -p "Digite o número de contato: " numero_contato
+# Solicitar números de contato ao usuário
+read -p "Digite os números de contato (separados por espaço): " numeros_contato
 
-# Verificar se o número de contato foi fornecido
-if [ -z "$numero_contato" ]; then
-    echo "Número de contato não fornecido. Saindo do script."
+# Verificar se os números de contato foram fornecidos
+if [ -z "$numeros_contato" ]; then
+    echo "Nenhum número de contato fornecido. Saindo do script."
     exit 1
 fi
 
 # Executar o loop para finalizar os contatos
-for i in $(php /home/futurofone/web/core/test/chats/contatos.php | grep "$numero_contato" | cut -d " " -f1); do
-    php /home/futurofone/web/core/cmd/chat/finalizarContato.php $i
+for numero_contato in $numeros_contato; do
+    for i in $(php /home/futurofone/web/core/test/chats/contatos.php | grep "$numero_contato" | cut -d " " -f1); do
+        php /home/futurofone/web/core/cmd/chat/finalizarContato.php $i
+    done
 done
 
 # Mensagem de conclusão
