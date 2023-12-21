518f7dc9db0e29cdbca8e7698e2c9f9754d82c26
Zuriel Amorim <38529015+zurielamorim@users.noreply.github.com>
2023-12-21 15:13:45 -0300

    Create sip_reload.sh

---
+++ sip_reload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Caminhos dos scripts PHP
+caminho_script1="/home/futurofone/scripts/new/ajustaAsteriskIncludes.php"
+caminho_script2="/home/futurofone/scripts/new/ajustaAsteriskConf.php"
+
+# Executa os scripts PHP
+php "${caminho_script1}"
+php "${caminho_script2}"
+
+# Recarrega os módulos IAX2 e SIP no Asterisk
+asterisk -rx 'iax2 reload'
+asterisk -rx 'sip reload'
+
+# Mensagem de sucesso
+echo "Todos os comandos foram executados com sucesso!"
