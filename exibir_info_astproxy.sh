f29f33ef319a92708b07e38904e22f1f8d5085d0
Zuriel Amorim <38529015+zurielamorim@users.noreply.github.com>
2024-07-23 14:54:09 -0300

    Update exibir_info_astproxy.sh

--- exibir_info_astproxy.sh
+++ exibir_info_astproxy.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-# Comando para exibir registros PJSIP
-echo "### PJSIP Show Registrations ###"
-docker exec -it astproxy asterisk -rx 'pjsip show registrations'
-echo "################################"
-echo ""
+# Solicita o número do tronco ao usuário
+read -p "Digite o número do tronco/recurso: " trunk_number
 
-# Comando para exibir pares IAX2
-echo "### IAX2 Show Peers ###"
-docker exec -it astproxy asterisk -rx 'iax2 show peers'
-echo "################################"
-echo ""
+# Executa o comando no Docker para mostrar os AORs
+aors_output=$(docker exec -it astproxy-$trunk_number asterisk -rx 'pjsip show aors')
 
-# Comando para exibir registros IAX2
-echo "### IAX2 Show Registry ###"
-docker exec -it astproxy asterisk -rx 'iax2 show registry'
-echo "################################"
+# Extrai o primeiro IP após "sip:"
+ip_address=$(echo "$aors_output" | grep -m 1 -oP 'sip:\K[0-9.]+')
+
+# Verifica se o IP foi encontrado
+if [ -z "$ip_address" ]; then
+    echo "Não foi possível encontrar o IP do primeiro AOR."
+    exit 1
+fi
+
+# Executa o comando no Docker para obter a rota IP
+route_output=$(docker exec -it astproxy-$trunk_number ip route get $ip_address)
+
+# Extrai o gateway da operadora (segundo IP na rota)
+gateway_ip=$(echo "$route_output" | awk '{print $3}')
+
+# Verifica se o gateway foi encontrado
+if [ -z "$gateway_ip" ]; then
+    echo "Não foi possível encontrar o gateway da operadora."
+    exit 1
+fi
+
+# Verifica a conectividade com o gateway da operadora
+ping -c 1 $gateway_ip &> /dev/null
+if [ $? -eq 0 ]; then
+    echo "O gateway da operadora está acessível."
+else
+    echo "O gateway da operadora está desligado."
+    exit 1
+fi
+
+# Verifica a conectividade com o SBC da operadora
+ping -c 1 $ip_address &> /dev/null
+if [ $? -eq 0 ]; then
+    echo "Comunicação com o SBC da operadora está ok."
+else
+    echo "Será necessário entrar em contato com a operadora."
+fi
